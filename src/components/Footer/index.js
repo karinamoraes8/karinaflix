9cdcdaf1835503b8e63254cb654abaf000ff2339
Karina Moraes <karina.moraes@mokedsystem.com>
2020-08-03 17:09:32 -0300

    Setup Aula 2 do Projeto

---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="./">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura 
+        </a>
+
+        <p>by <strong>KARINA MORAES</strong> </p> 
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
